dc3cdc54ce892b0867ade20fcdea86e31d7940b0
Sofiia Chorna <chernaya.sofiia@gmail.com>
2024-02-09 21:36:47 +0100

    td1: mask sobel - direct method

--- tp1/Convolutions.py
+++ tp1/Convolutions.py
@@ -4,12 +4,29 @@ from matplotlib import pyplot as plt
 
 SHARPEN_KERNEL = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
+MASK_SOBEL = {"x": np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]), "y": np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])}
+
 # Utilisé pour calculer l'indixe d'elément de la matrice
-matrix_diff = np.array([
+MATRIX_DIFF = np.array([
     (1, -1), (1, 0), (1, 1),
     (0, -1), (0, 0), (0, 1),
     (-1, -1), (-1, 0), (-1, 1)
 ])
+
+
+def calculate_gradient(img):
+    (h, w) = img.shape
+    i_x = cv2.copyMakeBorder(img, 1, 1, 1, 1, cv2.BORDER_REPLICATE)
+    i_y = cv2.copyMakeBorder(img, 1, 1, 1, 1, cv2.BORDER_REPLICATE)
+    for y in range(1, h - 1):
+        for x in range(1, w - 1):
+            val_x = np.sum([k * img[y + MATRIX_DIFF[i][0], x + MATRIX_DIFF[i][1]] for i, k in enumerate(MASK_SOBEL["x"])])
+            i_x[y, x] = min(max(val_x, 0), 255)
+
+            val_y = np.sum([k * img[y + MATRIX_DIFF[i][0], x + MATRIX_DIFF[i][1]] for i, k in enumerate(MASK_SOBEL["y"])])
+            i_y[y, x] = min(max(val_y, 0), 255)
+
+    return np.sqrt(i_x**2 + i_y**2)
 
 
 def get_image(path):
@@ -29,8 +46,7 @@ def apply_direct_method(img, kernel):
     for y in range(1, h - 1):
         for x in range(1, w - 1):
             # val = 5 * img[y, x] - img[y - 1, x] - img[y, x - 1] - img[y + 1, x] - img[y, x + 1]
-            val = np.sum([k * img[y + matrix_diff[i][0], x + matrix_diff[i][1]] for i, k in enumerate(kernel)])
-            print(val)
+            val = np.sum([k * img[y + MATRIX_DIFF[i][0], x + MATRIX_DIFF[i][1]] for i, k in enumerate(kernel)])
             img_convolution[y, x] = min(max(val, 0), 255)
     end_time = cv2.getTickCount()
 
@@ -42,10 +58,6 @@ def apply_direct_method(img, kernel):
     cv2.imshow('Avec boucle python',
                img_convolution.astype(np.uint8))  # Convention OpenCV : une image de type entier est interprétée dans {0,...,255}
     cv2.waitKey(0)
-
-    plt.subplot(121)
-    plt.imshow(img_convolution, cmap='gray')
-    plt.title('Convolution - Méthode Directe')
 
 
 def apply_filter2D(img, kernel):
@@ -63,16 +75,20 @@ def apply_filter2D(img, kernel):
                img_convolution / 255.0)  # Convention OpenCV : une image de type flottant est interprétée dans [0,1]
     cv2.waitKey(0)
 
-    plt.subplot(122)
-    plt.imshow(img_convolution, cmap='gray', vmin=0.0, vmax=255.0)  # Convention Matplotlib : par défaut, normalise l'histogramme !
-    plt.title('Convolution - filter2D')
-    plt.show()
 
-    # h_x = ([-1, 0, 1], [-2, 0, 2], [-1, 0, 1])
-    # h_y = np.transpose(h_x)
-    #
-    # img3 = cv2.filter2d(img, -1, h_x)
-    # img4 = cv2.filter2d(img, -1, h_y)
+def apply_mask_sobel(img):
+    start_time = cv2.getTickCount()
+    gradient = calculate_gradient(img)
+    end_time = cv2.getTickCount()
+
+    # Calculate le temps d'exécution
+    time = (end_time - start_time) / cv2.getTickFrequency()
+    title = "Mask Sobel (direct method)"
+    print(title, " - execution time:", time, "s")
+
+    # Afficher l'image avec la convolution
+    cv2.imshow(title, gradient.astype(np.uint8))
+    cv2.waitKey(0)
 
 
 image = get_image(path='Image_Pairs/FlowerGarden2.png')
@@ -81,3 +97,5 @@ print("Dimension de l'image :", h, "lignes x", w, "colonnes")
 
 apply_direct_method(image, kernel=SHARPEN_KERNEL)
 apply_filter2D(image, kernel=SHARPEN_KERNEL)
+apply_mask_sobel(image)
+
